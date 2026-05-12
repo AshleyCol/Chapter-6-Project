cf2405346fa74635680ad6012e0258fa7a5a677e
AshleyCol <ashe.coleman1022@gmail.com>
2026-05-12 14:51:44 -0400

    finished celsius table

--- Chapter 6 Project/Chapter 6 Project.cpp
+++ Chapter 6 Project/Chapter 6 Project.cpp
@@ -5,18 +5,18 @@ using namespace std;
 
 double getCelsius(double fahrenheit);
 
-const int fMin = 0;
-const int fMax = 20;
+const double fMin = 0;
+const double fMax = 20;
 
 // Handles the outputting of the table
 int main()
 {
     cout << "Fahrenheit to Celsius Table" << endl;
-    cout << setw(8) << "Fahrenheit" << setw(8) << "Celsius" << endl;
+    cout << setw(12) << "Fahrenheit" << setw(12) << "Celsius" << endl;
 
     for (double i = fMin; i <= fMax; i++) {
         cout << fixed << setprecision(1);
-        cout << right << setw(8) << i << setw(8) << getCelsius(i) << endl;
+        cout << right << setw(12) << i << setw(12) << getCelsius(i) << endl;
     }
 
     return 0;
@@ -24,7 +24,8 @@ int main()
 
 // Converts the fahrenheir value to celsius using the formula
 double getCelsius(double fahrenheit) {
-    double celsius = (5/9)(fahrenheit-32);
+
+    double celsius = (5.0/9.0) * (fahrenheit-32);
 
     return celsius;
 }
