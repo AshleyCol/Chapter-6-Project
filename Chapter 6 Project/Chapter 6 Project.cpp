b9d0d2e187b14ecd4b8c0a60b0ca720981100127
AshleyCol <ashe.coleman1022@gmail.com>
2026-05-12 15:39:04 -0400

    finished and pushed

--- Chapter 6 Project/Chapter 6 Project.cpp
+++ Chapter 6 Project/Chapter 6 Project.cpp
@@ -3,29 +3,84 @@
 
 using namespace std;
 
-double getCelsius(double fahrenheit);
+double getScore();
+bool isLower(double value1, double value2);
+bool isHigher(double value1, double value2);
+double calcAverage(double total, double minScore, double maxScore);
 
-const double fMin = 0;
-const double fMax = 20;
-
-// Handles the outputting of the table
+// Handles the running of the program
 int main()
 {
-    cout << "Fahrenheit to Celsius Table" << endl;
-    cout << setw(12) << "Fahrenheit" << setw(12) << "Celsius" << endl;
+    double current;
+    double finalScore;
+    double highest = 0.0;
+    double lowest = 10.0;
+    double totalScore = 0.0;
 
-    for (double i = fMin; i <= fMax; i++) {
-        cout << fixed << setprecision(1);
-        cout << right << setw(12) << i << setw(12) << getCelsius(i) << endl;
+    for (int i = 0; i < 5; i++) {
+        current = getScore();
+        totalScore = totalScore + current;
+
+
+        if (isLower(current, lowest)) {
+            lowest = current;
+        }
+
+        if (isHigher(current, highest)) {
+            highest = current;
+        }
     }
+
+    finalScore = calcAverage(totalScore, lowest, highest);
+
+    cout << fixed << setprecision(1) << "The star's final score is: " << finalScore;
 
     return 0;
 }
 
-// Converts the fahrenheir value to celsius using the formula
-double getCelsius(double fahrenheit) {
+// Gets a judges score from the user and validates it
+double getScore() {
+    double score;
+    for (bool valid = false; !valid;) {
+        cout << "What is this judge's score? ";
+        cin >> score;
 
-    double celsius = (5.0/9.0) * (fahrenheit-32);
+        if (score < 0.0 || score > 10.0) {
+            cout << "Invalid, please enter a number between 0-10" << endl;
+        }
+        else {
+            valid = true;
+        }
+    }
 
-    return celsius;
+    return score;
+}
+
+
+// Checks if a value is lower than the current lowest
+bool isLower(double value1, double value2) {
+    if (value1 < value2) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+
+// Checks if a value is higher than the current highest
+bool isHigher(double value1, double value2) {
+    if (value1 > value2) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+
+double calcAverage(double total, double minScore, double maxScore) {
+    double usedTotal = total - minScore - maxScore;
+
+    return (usedTotal / 3);
 }
