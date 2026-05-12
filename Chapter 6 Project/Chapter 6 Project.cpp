e32785c52775763c47e635775a6677323d715bae
AshleyCol <ashe.coleman1022@gmail.com>
2026-05-12 14:37:19 -0400

    push

--- Chapter 6 Project/Chapter 6 Project.cpp
+++ Chapter 6 Project/Chapter 6 Project.cpp
@@ -1,60 +1,30 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
+double getCelsius(double fahrenheit);
 
-void rectangle(int length,int width, int &perimeter, int &area);
-bool validateInput(int length, int width);
+const int fMin = 0;
+const int fMax = 20;
 
+// Handles the outputting of the table
 int main()
 {
-    bool check = false;
-    int userLength;
-    int userWidth;
-    int foundPerimeter;
-    int foundArea;
-    bool again;
+    cout << "Fahrenheit to Celsius Table" << endl;
+    cout << setw(8) << "Fahrenheit" << setw(8) << "Celsius" << endl;
 
-    //gets the user input and validates it
-    while (!check) {
-        cout << "What is the length of the rectangle? ";
-        cin >> userLength;
-        cout << "What is the width of the rectangle? ";
-        cin >> userWidth;
-
-        check = validateInput(userWidth, userLength);
-    }
-    
-    rectangle(userLength, userWidth, foundPerimeter, foundArea);
-
-
-    // gives the user the perimeter and area of the rectangle
-    cout << "The perimeter is " << foundPerimeter << endl;
-    cout << "The area is " << foundArea << endl;
-    cout << "Would you like to find another rectangle?" << endl << "0. End program" << endl << "1. Do another rectangle" << endl;
-    cin >> again;
-
-
-    // runs main again if the user chooses to
-    if (again) {
-        main();
+    for (double i = fMin; i <= fMax; i++) {
+        cout << fixed << setprecision(1);
+        cout << right << setw(8) << i << setw(8) << getCelsius(i) << endl;
     }
 
     return 0;
 }
 
-// makes sure the user did not input an invalid length or width
-bool validateInput(int length, int width) { 
-    if (length < 0 || width < 0) {
-        cout << "Invalid input, please try again." << endl;
-        return false;
-    }
-    else
-        return true;
-}
+// Converts the fahrenheir value to celsius using the formula
+double getCelsius(double fahrenheit) {
+    double celsius = (5/9)(fahrenheit-32);
 
-//calculates the area and perimeter of the user's rectangle
-void rectangle(int length, int width, int &perimeter, int &area) {
-    perimeter = 2 * (length + width);
-    area = length * width;
+    return celsius;
 }
